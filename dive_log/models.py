fe94350d7ff55c0b0c5774cbee24bfb54670584f
Christopher Ceder <cceder@ChristophersAir.localdomain>
2020-01-22 08:33:33 -0800

    not done yet

---
+++ dive_log/models.py
@@ -0,0 +1,13 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+class DiveLog(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=64)
+    body = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
